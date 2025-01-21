80b7898dec2c715cce7781c65bfdb0ed99f58982
ANIL <anilkumarbobba26@gmail.com>
2025-01-21 15:51:25 +0530

    first commit

---
+++ FIGMA TEMPLATE/tailwind.config.js
@@ -0,0 +1,22 @@
+
+tailwind.config = {
+    theme:{
+        extend:{
+            colors:{
+                red:'#FF0000'
+            },
+            left:{
+                anil:'54px'
+            },
+            top:{
+                we_build:'516px'
+            },
+            fontSize:{
+                'c_name_w':'292px'
+            },
+            fontSize:{
+                'c_name_h':'50px'
+            },
+        }
+    }
+  }
